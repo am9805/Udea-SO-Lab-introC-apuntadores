93c6c7a2c28e79a997cb8aa9d7ef45cf9adfb0cc
angelica <angelica@experimentality.com>
2020-03-12 22:45:33 -0500

    punto 7 terminado

--- soluciones/p5.7.c
+++ soluciones/p5.7.c
@@ -1,40 +1,79 @@
 #include <stdio.h>
-void shellsort(int arr[], int num)
+
+float mediaPorEstudiante(float *notas);
+void mediaPorMateria(float *notasAlejo, float *notasAna, float *notasPablo);
+float mediaTotal(float *notasAlejo, float *notasAna, float *notasPablo);
+
+int main()
 {
-    int i, j, k, tmp;
-    for (i = num / 2; i > 0; i = i / 2)
+
+    float alejandro[] = {5.0, 4.0, 3.0, 3.3, 4.5};
+    float ana[] = {3.5, 5.0, 4.0, 3.5, 4.0};
+    float pablo[] = {3.0, 4.5, 3.0, 4.0, 4.2};
+    float mediaAlejandro, mediaAna, mediaPablo, mediaTotalCurso;
+    mediaAlejandro = mediaPorEstudiante(alejandro);
+    printf("La media de las notas de Alejandro: %f\n", mediaAlejandro);
+    mediaAna = mediaPorEstudiante(ana);
+    printf("La media de las notas de Ana: %f\n", mediaAna);
+    mediaPablo = mediaPorEstudiante(pablo);
+    printf("La media de las notas de Pablo: %f\n", mediaPablo);
+    mediaPorMateria(alejandro, ana, pablo);
+    mediaTotalCurso = mediaTotal(alejandro, ana, pablo);
+    printf("La media total del curso es: %f\n", mediaTotalCurso);
+    return 0;
+}
+
+float mediaPorEstudiante(float *notas)
+{
+    float media;
+    float suma = 0;
+    for (int i = 0; i < 5; i++)
     {
-        for (j = i; j < num; j++)
+        suma = suma + notas[i];
+    }
+    media = suma / 5;
+    return media;
+}
+
+void mediaPorMateria(float *notasAlejo, float *notasAna, float *notasPablo)
+{
+    float media;
+    float suma = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        suma = notasAlejo[i] + notasAna[i] + notasPablo[i];
+        media = suma / 3;
+        switch (i)
         {
-            for(k = j - i; k >= 0; k = k - i)
-            {
-                if (arr[k+i] >= arr[k])
-                    break;
-                else
-                {
-                    tmp = arr[k];
-                    arr[k] = arr[k+i];
-                    arr[k+i] = tmp;
-                }
-            }
+        case 1:
+            printf("La media en notas de matemáticas es: %f\n", media);
+            break;
+        case 2:
+            printf("La media en notas de espanol es: %f\n", media);
+            break;
+        case 3:
+            printf("La media en notas de ingles es: %f\n", media);
+            break;
+        case 4:
+            printf("La media en notas de sociales es: %f\n", media);
+            break;
+        case 5:
+            printf("La media en notas de fisica es: %f\n", media);
+            break;
+        default:
+            break;
         }
     }
 }
-int main()
+
+float mediaTotal(float *notasAlejo, float *notasAna, float *notasPablo)
 {
-    int arr[300];
-    int k,  num;
-    printf("Ingrese el total de calificaciones : ");
-    scanf("%d", &num);
-    printf("\nIngrese %d calificaciones: ", num);
- 
-    for (k =  0 ; k < num; k++)
+    float media;
+    float suma = 0;
+    for (int i = 0; i < 5; i++)
     {
-        scanf("%d", &arr[k]);
+       suma = suma +  notasAlejo[i] + notasAna[i] + notasPablo[i];
     }
-    shellsort(arr, num);
-    printf("\n Sorted array is: ");
-    for (k = 0; k < num; k++)
-        printf("%d ", arr[k]);
-    return 0;
+    media = suma / 15;
+    return media;
 }
